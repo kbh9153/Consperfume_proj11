089a3b0afd26bcf82e72f7295648d2da50df8fc2
ByeongHyeon_Kim <kbh9153@gmail.com>
2022-03-29 00:06:56 +0900

    [Update]sign up validate

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,13 @@
-const cardLike = document.querySelector(".card-like");
+const cardLikes = document.querySelectorAll(".card-like");
 
 function handleLikeClick() {
    alert("로그인 후 관심상품 등록이 가능합니다.");
 }
 
-cardLike.addEventListener("click", handleLikeClick);
+cardLikes.forEach((Like) => {
+   Like.addEventListener("click", handleLikeClick);
+});
+
+// cardLikes.forEach(function (Like) {    // 구문 : arrays.forEach(callbackfunction(parameter) {실행 구문});
+//    Like.addEventListener("click", handleLikeClick);      // 구문 : addEventListener("Event type", Event Object)
+// });
---
+++ assets/js/signUpValidate.js
@@ -0,0 +1,71 @@
+const id = document.getElementById("id");
+const pw = document.getElementById("pw");
+const name = document.getElementById("name");
+const checkPw = document.getElementById("checkPw");
+const email = document.getElementById("email");
+const submit = document.getElementById("submit");
+
+const idCheck = /^[a-zA-Z0-9]{4,16}$/;
+const pwCheck = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
+const emailCheck = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+
+// signUpCheck 유효성 검사
+function signUpCheck() {
+   // id
+
+   if (id.value == "") {
+      alert("아이디를 입력하세요.");
+      id.focus();
+      return false;
+   }
+
+   if (!idCheck.test(id.value)) {
+      alert("아이디는 영문 대소문자 + 숫자 4 ~ 16자 조합으로 입력해야합니다.");
+      return false;
+   }
+
+   if (pw.value == "") {
+      alert("비밀번호를 입력하세요.");
+      return false;
+   }
+
+   // password
+
+   if (!pwCheck.test(pw.value)) {
+      alert("비밀번호는 영문 대소문자 + 숫자 + 특수문자 조합으로 8 ~ 16자리 입력해야합니다.");
+      pw.focus();
+      return false;
+   }
+
+   if (checkPw.value !== pw.value) {
+      alert("비밀번호가 일치하지 않습니다.");
+      checkPw.focus();
+      return false;
+   }
+
+   // name
+
+   if (name.value == "") {
+      alert("이름을 입력하세요.");
+      name.focus();
+      return false;
+   }
+
+   // email
+
+   if (email.value == "") {
+      alert("이메일 주소를 입력하세요.");
+      email.focus();
+      return false;
+   }
+
+   if (!emailCheck.test(email.value)) {
+      alert("이메일 형식에 알맞지 않습니다.");
+      return false;
+   }
+
+   // submit !!
+   document.join_form.submit();
+}
+
+submit.addEventListener("click", signUpCheck);
